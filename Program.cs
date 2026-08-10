f1c0a3f73d58a02f3960071f675a21637973c878
tomas912g <tgutierrezofficial@gmail.com>
2026-08-10 10:10:46 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -3,3 +3,9 @@ Console.WriteLine($"=== {nombre_kiosko} ===");
 Console.Write("Nombre del cajero: ");
 string nombre_cajerx = Console.ReadLine();
 Console.WriteLine($"Bienvenido, {nombre_cajerx}. Caja abierta.");
+
+Console.Write("Nombre del producto: ");
+string nombreProducto = Console.ReadLine();
+Console.Write("Precio del producto: ");
+decimal precioProducto = decimal.Parse(Console.ReadLine());
+Console.WriteLine($"El producto: {nombreProducto}, cuesta {precioProducto}");
